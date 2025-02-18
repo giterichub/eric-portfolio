987b4e439c5c98b682966f4357df9cf5589a491d
eric antao <ericantao99@gmail.com>
2025-02-18 16:58:35 +0530

    Navbar component floating with styles

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+            <li><a href="#section1">HOME</a><div className="progress-bar"></div></li>
+            <li><a href="#section2">ABOUT</a><div className="progress-bar"></div></li>
+            <li><a href="#section3">TIMELINE</a><div className="progress-bar"></div></li>
+            <li><a href="#section4">PORTFOLIO</a><div className="progress-bar"></div></li>
+            <li><a href="#section5">CONTACT</a><div className="progress-bar"></div></li>
+            </ul>
+        </div>
+    );
+} 
+export default Navbar;
