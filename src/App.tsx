87973041e8e696f96cf276ee5244156baf65a65d
eric antao <ericantao99@gmail.com>
2025-02-20 15:43:58 +0530

    Imported all basic sections to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar';
+import Intro from './sections/Intro';
+import About from './sections/About';
+import Timeline from './sections/Intro';
+import Portfolio from './sections/Portfolio';
+import Contact from './sections/Contact';
 import './styles/navbar.css';
 import './App.css'
 
@@ -9,6 +14,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
+      <About />
+      <Timeline />
+      <Portfolio />
+      <Contact />
     </div>
   )
 }
