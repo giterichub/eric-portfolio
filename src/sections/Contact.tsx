a4ae489a106cd1fde8a924e2f2df3a86d6b154a7
eric antao <ericantao99@gmail.com>
2025-02-20 15:36:17 +0530

    Basic contact section structure

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,4 +1,8 @@
 const Contact = () => {
-    return <div>Timeline Section</div>
+    return (
+        <div id="section5" className="section section5">
+            <p>Section 5: Purple Background</p>
+        </div>
+    );
 } 
 export default Contact; 
