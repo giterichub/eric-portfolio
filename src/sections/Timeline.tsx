5e2d55f92cfdd738c0937828afa2922c30e781b1
eric antao <ericantao99@gmail.com>
2025-02-20 15:27:53 +0530

    Timeline basic structure section update

--- src/sections/Timeline.tsx
+++ src/sections/Timeline.tsx
@@ -1,4 +1,8 @@
 const Timeline = () => {
-    return <div>Timeline Section</div>
+    return (
+        <div id="section3" className="section section3">
+            <p>Section 3: Green Background</p>
+        </div>
+    );
 } 
 export default Timeline;
