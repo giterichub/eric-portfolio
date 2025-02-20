bfd1d6dbe4ade2d9510e8b79d39f88a8df7d7b55
eric antao <ericantao99@gmail.com>
2025-02-20 15:49:11 +0530

    changed timeline id name

--- src/sections/Timeline.tsx
+++ src/sections/Timeline.tsx
@@ -1,6 +1,6 @@
 const Timeline = () => {
     return (
-        <div id="section3" className="section section3">
+        <div id="timeline" className="section section3">
             <p>Section 3: Green Background</p>
         </div>
     );
