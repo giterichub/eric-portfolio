b98f50229f25795a2b098aad8512e61ded6c4fae
eric antao <ericantao99@gmail.com>
2025-02-22 17:08:20 +0530

    Created contact section content

---
+++ src/content/contact.ts
@@ -0,0 +1,10 @@
+export const contactContent = {
+    socialLinks: [
+      { name: "LinkedIn", url: "www.linkedin.com/in/eric-antao" },
+      { name: "Github", url: "https://github.com/giterichub" },
+      { name: "Portfolio", url: "https://giterichub.github.io/My-Portfolio/" },
+      { name: "ericantao99@gmail.com", url: "ericantao99@gmail.com" },
+      { name: "7798685915", url: "7798685915" },
+    ]
+  };
+  
