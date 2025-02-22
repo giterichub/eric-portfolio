b87d957efe31c70932e278dc225e8b6d41fa8a93
eric antao <ericantao99@gmail.com>
2025-02-22 17:33:17 +0530

    Updated content file for contact social links

--- src/content/contact.ts
+++ src/content/contact.ts
@@ -3,8 +3,8 @@ export const contactContent = {
       { name: "LinkedIn", url: "www.linkedin.com/in/eric-antao" },
       { name: "Github", url: "https://github.com/giterichub" },
       { name: "Portfolio", url: "https://giterichub.github.io/My-Portfolio/" },
-      { name: "ericantao99@gmail.com", url: "ericantao99@gmail.com" },
-      { name: "7798685915", url: "7798685915" },
+      { name: "ericantao99@gmail.com", url: "mailto:ericantao99@gmail.com" },
+      { name: "+91 7798685915", url: "tel:+917798685915" },
     ]
   };
   
