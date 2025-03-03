e60503fbc77865230215c03fbb7f94f9017ee070
eric antao <ericantao99@gmail.com>
2025-03-04 01:29:53 +0530

    Overlapping circles animation

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -35,6 +35,18 @@ const Home = () => {
 
       {/* <div className="background-overlay"></div> */}
 
+      <div className="ov-circle-container">
+        <svg width="200" height="200" viewBox="0 0 200 200">
+          <g transform="rotate(-20, 100, 100)">
+            <circle className="overlap-circle" cx="50" cy="100" r="30" stroke="#d21338" fill="none" stroke-width="2" />
+            <circle className="overlap-circle" cx="65" cy="95" r="30" stroke="#d21338" fill="none" stroke-width="2" />
+            <circle className="overlap-circle" cx="80" cy="90" r="30" stroke="#d21338" fill="none" stroke-width="2" />
+            <circle className="overlap-circle" cx="95" cy="85" r="30" stroke="#d21338" fill="none" stroke-width="2" />
+            <circle className="overlap-circle" cx="110" cy="80" r="30" stroke="#d21338" fill="none" stroke-width="2" />
+          </g>
+        </svg>
+      </div>
+
       <div className="content">
         <h1>Eric Antao</h1>
         <h2>Frontend Developer</h2>
