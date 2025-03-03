9eb7a4d7e6760a9b826b927590e208fca37635e3
eric antao <ericantao99@gmail.com>
2025-03-04 01:33:56 +0530

    Overlapping triangles SVG

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -89,6 +89,19 @@ const Home = () => {
       </div>
       <div className="styled-box"></div>
 
+      {/* SVG: Overlapping Triangles */}
+      <div className="ov-triangle-container">
+        <svg width="200" height="200" viewBox="0 0 100 200">
+          <g transform="rotate(20, 50, 80)">
+            <polygon points="50,10 10,80 90,80" stroke="red" fill="none" stroke-width="2" />
+            <polygon points="50,20 15,85 85,85" stroke="red" fill="none" stroke-width="2" />
+            <polygon points="50,30 20,90 80,90" stroke="red" fill="none" stroke-width="2" />
+            <polygon points="50,40 25,95 75,95" stroke="red" fill="none" stroke-width="2" />
+            <polygon points="50,50 30,100 70,100" stroke="red" fill="none" stroke-width="2" />
+          </g>
+        </svg>
+      </div>
+
       <div className="circle-rotate-container">
         <AnimatedCircleButton onClick={handleButtonClick} />
       </div>
