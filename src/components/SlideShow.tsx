d4ee6db6bb684c2d6a4ec0f56b40d2d951115407
eric antao <ericantao99@gmail.com>
2025-03-08 14:29:16 +0530

    Updated slideshow

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -1,36 +1,30 @@
 import '../styles/slideshow.css';
-import { slideShowContent } from '../content/slideshow';
+import { slideShowContent, Image } from '../content/slideshow';
 import { useEffect, useState } from 'react';
-
-interface Image {
-    name: string;
-    alt: string;
-    url: string;
-  }
-  
-  interface SlideShowContent {
-    imageLinks: Image[];
-  }
 
 const SlideShow = () => {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const { imageLinks }: { imageLinks: Image[] } = slideShowContent;
+
 useEffect(() => {
       const intervalId = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % slideShowContent.imageLinks.length);
       }, 3000);
   
       return () => clearInterval(intervalId);
-    }, [slideShowContent.imageLinks.length]);
+    }, [imageLinks.length]);
     return (
         <div className="carousel-container">
-                      {slideShowContent.imageLinks.map((image, index) => (
+                      {imageLinks.map((image, index) => (
                         <img
                           key={index}
                           className={`carousel-image ${
                             currentIndex === index ? "active" : ""
                           }`}
                           src={image.url}
-                          alt={`Image ${index + 1}`}
+                          alt={image.alt}
+                          aria-hidden={currentIndex !== index}
+                            loading="lazy"
                         />
                       ))}
                     </div>
