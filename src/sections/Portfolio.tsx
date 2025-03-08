6ae1dccf942bf5b337110d710f2cc6e31d4f899c
eric antao <ericantao99@gmail.com>
2025-03-08 14:37:13 +0530

    Added slideshow component

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import '../styles/portfolio.css'
+import SlideShow from '../components/SlideShow';
 
 const Portfolio = () => {
     const [rectWidth, setRectWidth] = useState<number>(100);
@@ -55,22 +56,6 @@ const Portfolio = () => {
       return () => window.removeEventListener("resize", updateLayout);
     }, []);
   
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
-  
-    const images = [
-      "https://images.unsplash.com/photo-1741290723082-bd54c16a21a8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://plus.unsplash.com/premium_photo-1730032452988-c8d4df1256c5?q=80&w=1466&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1741070487520-907d1359cb95?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    ];
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 3000);
-  
-      return () => clearInterval(intervalId);
-    }, [images.length]);
-  
     return (
       <div className="section section4">
         <section className="projects pt-35 pb-250">
@@ -84,18 +69,7 @@ const Portfolio = () => {
                 <span className="wo">WO</span>
                 <span className="rect" style={{ width: `${rectWidth}px`, height: `${rectHeight}px` }}>
                   <div className="outer-container" id="outer-container" style={{ width: `${rectWidth}px`, height: `${rectHeight}px` }}>
-                    <div className="carousel-container">
-                      {images.map((src, index) => (
-                        <img
-                          key={index}
-                          className={`carousel-image ${
-                            currentIndex === index ? "active" : ""
-                          }`}
-                          src={src}
-                          alt={`Image ${index + 1}`}
-                        />
-                      ))}
-                    </div>
+                    <SlideShow />
                   </div>
                 </span>
                 <span className="rk">RK</span>
