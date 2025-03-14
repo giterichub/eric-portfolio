6a6160052943565c56a5a12efd582c12850664d9
eric antao <ericantao99@gmail.com>
2025-03-14 18:44:48 +0530

    Updated content info

--- src/content/projectContent.ts
+++ src/content/projectContent.ts
@@ -20,17 +20,43 @@ export interface Project {
   
   export const projects: Project[] = [
     {
-      id: 'project-1',
-      title: 'E-commerce Platform',
-      description: 'A modern shopping experience...',
+      id: 'game-hub',
+      title: 'Game Hub',
+      description: 'Find all the best games...',
       images: [
         'https://.../proj1-1.jpg',
         'https://.../proj1-2.jpg'
       ],
-      featuredImage: 'https://.../proj1-main.jpg',
+      featuredImage: 'https://images.unsplash.com/photo-1741290723082-bd54c16a21a8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       isFeatured: true,
       showInSlideshow: true,
       backgroundColor: '#2a2a2a'
     },
+    {
+        id: 'movie-app',
+        title: 'Movie App',
+        description: 'A modern platform to get all the latest movies...',
+        images: [
+          'https://.../proj1-1.jpg',
+          'https://.../proj1-2.jpg'
+        ],
+        featuredImage: 'https://plus.unsplash.com/premium_photo-1730032452988-c8d4df1256c5?q=80&w=1466&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        isFeatured: true,
+        showInSlideshow: true,
+        backgroundColor: '#2a2a2a'
+      },
+      {
+        id: 'crosshair',
+        title: 'Crosshair',
+        description: 'A modern shopping experience...',
+        images: [
+          'https://.../proj1-1.jpg',
+          'https://.../proj1-2.jpg'
+        ],
+        featuredImage: 'https://images.unsplash.com/photo-1741070487520-907d1359cb95?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        isFeatured: true,
+        showInSlideshow: true,
+        backgroundColor: '#2a2a2a'
+      }
     // Other projects...
   ];
