dcd790cda4a1cbfa96b194f93bd43178035ebb50
eric antao <ericantao99@gmail.com>
2025-08-15 15:04:56 +0530

    Removed svg container div, added svg css

--- src/svgs/OverlappingCircles/OverlappingCircles.tsx
+++ src/svgs/OverlappingCircles/OverlappingCircles.tsx
@@ -1,6 +1,5 @@
 const OverlappingCircles = () => {
     return (
-        <div className="ov-circle-container">
             <svg width="200" height="200" viewBox="0 0 200 200">
                 <g transform="rotate(-20, 100, 100)">
                     <circle className="overlap-circle" cx="50" cy="100" r="30" stroke="#d21338" fill="none" stroke-width="2" />
@@ -10,7 +9,6 @@ const OverlappingCircles = () => {
                     <circle className="overlap-circle" cx="110" cy="80" r="30" stroke="#d21338" fill="none" stroke-width="2" />
                 </g>
             </svg>
-      </div>
     );
 }
 export default OverlappingCircles;
