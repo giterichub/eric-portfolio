3758f1cf819be60b77fda48e7737077f98d5ce98
eric antao <ericantao99@gmail.com>
2025-08-25 10:49:07 +0530

    Formatted code\

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -10,35 +10,6 @@ const allSlideshowImages = [
   ])
 ];
 
-// const imageUrls = [
-//   'https://images.unsplash.com/photo-1741290723082-bd54c16a21a8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//   'https://plus.unsplash.com/premium_photo-1730032452988-c8d4df1256c5?q=80&w=1466&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-//   'https://images.unsplash.com/photo-1741070487520-907d1359cb95?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-// ];
-
-// const sections = [
-//   {
-//     bgImage: imageUrls[0],
-//     title: 'Short CV',
-//     text: 'With a strategic brand and digital product development background...',
-//     button: 'LinkedIn CV'
-//   },
-//   {
-//     bgImage: imageUrls[1],
-//     title: 'Experience',
-//     text: 'Extensive experience working with global brands...',
-//     button: 'View Portfolio'
-//   },
-//   {
-//     bgImage: imageUrls[2],
-//     title: 'Projects',
-//     text: 'Extensive Projects working with global brands...',
-//     button: 'View Portfolio'
-//   }
-// ];
-// const projectSections = Array.from({ length: 4 });
-
-
 const Portfolio = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const projectsWordRef = useRef<HTMLSpanElement>(null);
